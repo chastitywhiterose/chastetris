f407d78748c62c22d072a836c2739688c85445c0
Chastity White Rose <chastitywhiterose@gmail.com>
2022-05-12 15:00:10 -0500

    Script to build on Windows

---
+++ src/raymake.bat
@@ -0,0 +1,11 @@
+SET RAYLIB_PATH=C:\raylib\raylib
+SET COMPILER_PATH=C:\raylib\w64devkit\bin
+SET PATH=%COMPILER_PATH%
+SET CC=C:\raylib\w64devkit\bin\gcc
+SET CFLAGS=%RAYLIB_PATH%\src\raylib.rc.data -s -static -Os -std=c99 -Wall -I%RAYLIB_PATH%\src -Iexternal -DPLATFORM_DESKTOP
+SET LDFLAGS=-lraylib -lopengl32 -lgdi32 -lwinmm
+
+%CC% -o main.exe main.c %CFLAGS% %LDFLAGS%
+main
+
+
