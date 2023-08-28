86851080939f6b529760d569b7e07947b78c4a5b
Chastity White Rose <chastitywhiterose@gmail.com>
2023-08-27 22:17:14 -0500

    static and dynamic windows build scripts available

--- src/SDL_Renderer/w64_sdl_make.bat
+++ src/SDL_Renderer/w64_sdl_make.bat
@@ -3,3 +3,4 @@ SET COMPILER_PATH=%DEVKIT%\bin
 SET PATH=%COMPILER_PATH%
 
 gcc -Wall -ansi -pedantic main.c -o main -I%DEVKIT%\include\SDL2 -L%DEVKIT%\lib -lmingw32 -lSDL2main -lSDL2 && main
+
---
+++ src/SDL_Renderer/w64_sdl_make_static.bat
@@ -0,0 +1,6 @@
+SET DEVKIT=C:\w64devkit
+SET COMPILER_PATH=%DEVKIT%\bin
+SET PATH=%COMPILER_PATH%
+
+gcc -Wall -ansi -pedantic main.c -o main -I%DEVKIT%\include\SDL2 -L%DEVKIT%\lib -lmingw32 -lSDL2main %DEVKIT%\lib\libSDL2.a -mwindows -Wl,--dynamicbase -Wl,--nxcompat -Wl,--high-entropy-va -lm -ldinput8 -ldxguid -ldxerr8 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lshell32 -lsetupapi -lversion -luuid && main
+
