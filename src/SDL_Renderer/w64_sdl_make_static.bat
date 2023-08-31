29bee3098005f333d29cd0fc430c92760a1c780d
Chastity White Rose <chastitywhiterose@gmail.com>
2023-08-31 11:05:25 -0500

    Add files via upload

--- src/SDL_Renderer/w64_sdl_make_static.bat
+++ src/SDL_Renderer/w64_sdl_make_static.bat
@@ -4,3 +4,4 @@ SET PATH=%COMPILER_PATH%
 
 gcc -Wall -ansi -pedantic main.c -o main -I%DEVKIT%\include\SDL2 -L%DEVKIT%\lib -lmingw32 -lSDL2main %DEVKIT%\lib\libSDL2.a -mwindows -Wl,--dynamicbase -Wl,--nxcompat -Wl,--high-entropy-va -lm -ldinput8 -ldxguid -ldxerr8 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lshell32 -lsetupapi -lversion -luuid && main
 
+strip main.exe
